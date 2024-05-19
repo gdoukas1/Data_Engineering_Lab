f71caf6610be966ee2461813379ebb6c5456de07
Giorgos Doukas <44439836+gdoukas1@users.noreply.github.com>
2024-05-19 18:43:18 +0300

    Add files via upload

---
+++ Project_1/create_insert_example.sql
@@ -0,0 +1,21 @@
+--Create database
+CREATE DATABASE Cataschevastica
+use Cataschevastica
+
+-- INSERTS
+INSERT INTO Supplier VALUES ('titan','691234567')
+INSERT INTO Supplier VALUES ('SGB','276108852')
+
+INSERT INTO RawMaterial VALUES ('wood', 1)
+
+INSERT INTO ProductMaterials VALUES (10001, 5)
+INSERT INTO ProductMaterials VALUES (10001, 1)
+
+
+
+
+SELECT * FROM Supplier
+
+SELECT * FROM RawMaterial
+SELECT * FROM Product
+
